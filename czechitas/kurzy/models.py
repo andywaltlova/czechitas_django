01fad485fed49b990eeca923c1a07b166e4731c6
Andrea Waltlova <x449831@fi.muni.cz>
2021-04-20 18:30:53 +0200

    Add first part of 10th lecture

--- czechitas/kurzy/models.py
+++ czechitas/kurzy/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Kurz(models.Model):
-  nazev = models.CharField(max_length=100)
-  zacatek = models.DateTimeField()
-  konec = models.DateTimeField()
-  popis = models.CharField(max_length=1000)
-  cena = models.IntegerField()
+    nazev = models.CharField(max_length=100)
+    zacatek = models.DateTimeField()
+    konec = models.DateTimeField()
+    popis = models.CharField(max_length=1000)
+    cena = models.IntegerField()
--- czechitas/kurzy/views.py
+++ czechitas/kurzy/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import ListView
+from . import models
 
 
 class MujPrvniPohled(View):
     def get(self, request):
         return HttpResponse('Vítej na webu Czechitas!')
+
+
+class KurzyView(ListView):
+    model = models.Kurz
+    template_name = "kurzy/kurzy_list.html"
