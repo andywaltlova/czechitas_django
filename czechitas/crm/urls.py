e9723b9ba7d82a4b731c776a0c61c8d0a66346df
Andrea Waltlova <andywaltlova@gmail.cz>
2021-05-04 19:42:06 +0200

    Added form for organization

--- czechitas/crm/urls.py
+++ czechitas/crm/urls.py
@@ -5,10 +5,28 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('kontakty/', views.KontaktyView.as_view(), name='kontakty'),
-    path('kontakty/<int:pk>', views.DetailKontaktView.as_view(), name='detail_kontakt'),
+    path('kontakty/<int:pk>', views.DetailKontaktView.as_view(),
+         name='detail_kontakt'),
     path('organizace/', views.OrganizaceView.as_view(), name='organizace'),
-    path('organizace/<int:pk>', views.DetailOrganizaceView.as_view(), name='detail_organizace'),
-    path('kontakty/pridani', views.VytvorKontakt.as_view(), name='pridani_kontaktu'),
-    path("organizace/<int:pk>/vytvor-kontakt", views.VytvorKontaktOrganizaci.as_view(), name="vytvor_kontakt_k_organizaci"),
-    path('kontakty/potvrzeni', views.PotvrzeniKontaktu.as_view(), name='potvrzeni_kontaktu'),
+    path('organizace/<int:pk>', views.DetailOrganizaceView.as_view(),
+         name='detail_organizace'),
+
+    path('kontakty/pridani',
+         views.VytvorKontakt.as_view(),
+         name='pridani_kontaktu'),
+
+    path('organizace/pridani',
+         views.VytvorOrganizaci.as_view(),
+         name='pridani_organizace'),
+
+    path('organizace/potvrzeni',
+         views.PotvrzeniOrganizace.as_view(),
+         name='potvrzeni_organizace'),
+
+    path("organizace/<int:pk>/vytvor-kontakt",
+         views.VytvorKontaktOrganizaci.as_view(),
+         name="vytvor_kontakt_k_organizaci"),
+    path('kontakty/potvrzeni',
+         views.PotvrzeniKontaktu.as_view(),
+         name='potvrzeni_kontaktu'),
 ]
--- czechitas/crm/views.py
+++ czechitas/crm/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from . import models
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -23,13 +24,16 @@ class KontaktyView(ListView):
     model = models.Kontakt
     template_name = "crm/crm_kontakty.html"
 
+
 class DetailKontaktView(DetailView):
     model = models.Kontakt
     template_name = "crm/detail_kontaktu.html"
 
+
 class OrganizaceView(ListView):
     model = models.Organizace
     template_name = "crm/crm_organizace.html"
+
 
 class DetailOrganizaceView(DetailView):
     model = models.Organizace
@@ -39,13 +43,24 @@ class DetailOrganizaceView(DetailView):
 class VytvorKontakt(CreateView):
     model = models.Kontakt
     template_name = "crm/kontakt/pridani.html"
-    fields = ["jmeno", "prijmeni", "email", "datum_posledniho_kontaktu", "organizace"]
+    fields = ["jmeno", "prijmeni", "email", "datum_posledniho_kontaktu",
+              "organizace"]
     success_url = reverse_lazy('potvrzeni_kontaktu')
 
 
 class PotvrzeniKontaktu(TemplateView):
     template_name = "crm/kontakt/potvrzeni.html"
 
+
+class VytvorOrganizaci(CreateView):
+    model = models.Organizace
+    template_name = "crm/organizace/pridani.html"
+    fields = ["jmeno", "ico", "ulice", "mesto", "psc"]
+    success_url = reverse_lazy('potvrzeni_organizace')
+
+
+class PotvrzeniOrganizace(TemplateView):
+    template_name = "crm/organizace/potvrzeni.html"
 
 
 
@@ -64,6 +79,3 @@ class VytvorKontaktOrganizaci(CreateView):
         organizace = models.Organizace.objects.get(pk=id_organizace)
         form.instance.organizace = organizace
         return super().form_valid(form)
-
-
-
